bedd563b29f68deb347d5c1ab0109747cfb2eb69
jameskim <jameskim9509@github.com>
2024-12-26 21:27:12 +0900

    LectureController 구현

---
+++ src/main/java/com/hhplus/ddd/controller/LectureController.java
@@ -0,0 +1,33 @@
+package com.hhplus.ddd.controller;
+
+import com.hhplus.ddd.application.LectureApplyApplication;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@RestController
+@RequiredArgsConstructor
+public class LectureController {
+    private final LectureApplyApplication lectureApplyApplication;
+
+    @PostMapping("/{lectureId}/users/{userId}")
+    public Object applyLecture(
+            @PathVariable("userId") Long userId,
+            @PathVariable("lectureId") Long lectureId
+    ){
+        return lectureApplyApplication.applyLecture(userId, lectureId);
+    }
+
+    @GetMapping
+    public Object getLectures(@RequestParam("date") LocalDate date){
+        return lectureApplyApplication.getLecturesByDate(date);
+    }
+
+    @GetMapping("/completion/users/{userId}")
+    public Object getCompleted(
+            @PathVariable("userId") Long userId
+    ){
+        return lectureApplyApplication.getCompletedByUserId(userId);
+    }
+}
